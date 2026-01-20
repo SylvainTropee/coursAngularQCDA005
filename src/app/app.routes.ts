890fe7677c2b3be22ab9636d4bc315f2a4ce6ee0
stropee <stropee@campus-eni.fr>
2026-01-20 14:07:52 +0100

    mod 5 demo 3 guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import {Mod5home} from './mod5/mod5home/mod5home';
 import {Mod5about} from './mod5/mod5about/mod5about';
 import {Mod5detail} from './mod5/mod5detail/mod5detail';
+import {authGuard} from './mod5/auth-guard';
 
 export const routes: Routes = [
   {path : '', component : Mod5home,},
   {path : 'about-us', component : Mod5about},
-  {path : 'detail/:id', component : Mod5detail},
+  {path : 'detail/:id', component : Mod5detail, canActivate : [authGuard]},
   {path : '**', component : Mod5home},
 ];
---
+++ src/app/mod5/auth-guard.ts
@@ -0,0 +1,18 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {Mod4service} from '../mod4/mod4service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(Mod4service)
+  const router = inject(Router)
+
+  if(authService.isLogged()){
+    return true;
+  }else{
+    router.navigate(["/about-us"], {queryParams : {error : 1}})
+    return false
+  }
+
+
+};
